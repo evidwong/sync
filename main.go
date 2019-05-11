0c5dbde38fbf17a4abd34443e695282fdcddf5ed
Evid Wong <76572677@qq.com>
2019-05-11 18:41:20 +0800

    d

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"sync"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/silenceper/wechat/cache"
 
@@ -109,141 +110,166 @@ func copyConfigToContext(cfg *Config, context *context.Context) {
  * 开启时查询所有的商户，并加入到时服务中
  */
 func run(mydb *Mydb) {
-	logFileName := "./logs/" + time.Now().Format("2006-01-02") + ".log"
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	logFileName := "./logs/system.log"
 	logFileHandle, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE, 666)
 	if err != nil {
-		log.Fatalln("fail to create " + logFileName + " file!")
+		log.Panicln("fail to create " + logFileName + " file!")
 	}
-	defer logFileHandle.Close()
-	logger = log.New(logFileHandle, "", log.LstdFlags|log.Lshortfile) // 日志文件格式:log包含时间及文件行数
+	log.SetOutput(logFileHandle)
+	logFileHandle.Close()
 	for i := 0; i < 1; i++ {
 		go sendWechatTemplateMessage(mydb)
 		go sendSmsMessage(mydb)
 	}
 }
 
-func sendWechatTemplateMessage(mydb *Mydb) {
-	var cid int
-	for {
-		r := pool.Conn.Get()
-		_, err := r.Do("Select", 10)
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 1)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		key, err := redis.String(r.Do("Rpop", "wechat:message:template"))
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 2)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		content, err := redis.String(r.Do("Get", key))
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 3)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		var redisData WechatMessage
-		err = json.Unmarshal([]byte(content), &redisData)
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 4)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		var msg template.Message
-		err = json.Unmarshal([]byte(redisData.Job), &msg)
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 6)
-			time.Sleep(time.Second * 2)
-			continue
-		}
+// GetInfo 获取redis
+func GetInfo(t string, k string) (redis.Conn, *WechatMessage, *os.File, string) {
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	filename := "./logs/" + t + "_" + time.Now().Format("2006-01-02") + ".log"
+	logFileHandle, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 666)
+	if err != nil {
+		log.Info("创建或使用日志文件失败. " + err.Error())
+		time.Sleep(time.Second * 5)
+		return nil, nil, nil, ""
+	}
+	log.SetOutput(logFileHandle)
 
-		cid = redisData.Cid
-		_, err = r.Do("Select", 1)
-		if err != nil {
-			r.Close()
-			fmt.Println(err, 7)
-			time.Sleep(time.Second * 2)
+	r := pool.Conn.Get()
+	_, err = r.Do("Select", 10)
+	if err != nil {
+		r.Close()
+		log.Info("选择数据库 10 失败. " + err.Error())
+		time.Sleep(time.Second * 3)
+		return nil, nil, nil, ""
+	}
+	key, err := redis.String(r.Do("Rpop", k))
+	if err != nil {
+		r.Close()
+		log.Info("Rpop列表 失败. " + err.Error())
+		time.Sleep(time.Second * 3)
+		return nil, nil, nil, ""
+	}
+	content, err := redis.String(r.Do("Get", key))
+	if err != nil {
+		r.Close()
+		log.Info("获取key对应的数据 失败. " + err.Error())
+		return nil, nil, nil, ""
+	}
+	var redisData WechatMessage
+	err = json.Unmarshal([]byte(content), &redisData)
+	if err != nil {
+		r.Close()
+		log.Info("JSON解析key对应的数据 失败. " + err.Error())
+		time.Sleep(time.Second * 3)
+		return nil, nil, nil, ""
+	}
+	return r, &redisData, logFileHandle, key
+
+}
+func sendWechatTemplateMessage(mydb *Mydb) {
+	var cid string
+	for {
+		r, job, file, key := GetInfo("wechat", "wechat:message:template")
+		if r == nil {
+			time.Sleep(time.Second * 5)
 			continue
 		}
-		wechatConfig, err := redis.StringMap(r.Do("HgetAll", "wechat_config:"+strconv.Itoa(cid)))
+		_, err := r.Do("Select", 1)
 		if err != nil {
 			r.Close()
-			fmt.Println(err, 8)
+			log.Info("切换数据库到 1 失败. " + err.Error())
+			file.Close()
+			time.Sleep(time.Second * 5)
+			continue
+		}
+		cid = strconv.Itoa(job.Cid)
+		cfg, err := redis.StringMap(r.Do("HgetAll", "wechat_config:"+cid))
+		if err != nil {
+			r.Close()
+			log.Info("[" + cid + "] 获取配置信息 失败. " + err.Error())
+			file.Close()
+			time.Sleep(time.Second * 3)
 			continue
 		}
 		wechat := NewWechat(&Config{
-			AppID:     wechatConfig["appid"],
-			AppSecret: wechatConfig["appsecret"],
-			Token:     wechatConfig["token"],
+			AppID:     cfg["appid"],
+			AppSecret: cfg["appsecret"],
+			Token:     cfg["token"],
 			Cache:     pool,
 		})
+		var msg template.Message
+		err = json.Unmarshal([]byte(job.Job), &msg)
+		if err != nil {
+			r.Close()
+			log.Info("[" + cid + "] JSON解析微信模板消息 失败. " + err.Error())
+			file.Close()
+			time.Sleep(time.Second * 2)
+			continue
+		}
 		tplMsg := template.NewTemplate(wechat.Context)
 		_, err = tplMsg.Send(&msg)
 		if err != nil {
-			fmt.Println("发送模板消息失败")
-			fmt.Println(err, 9)
+			log.Info("[" + cid + "] 发送微信模板消息 失败. " + err.Error())
 			mydb.db.Table("remind_job").Where("redis_key_index=?", key).Update(map[string]interface{}{"status": -1, "fail_content": err})
 		} else {
 			mydb.db.Table("remind_job").Where("redis_key_index=?", key).Update(map[string]interface{}{"action_at": time.Now().Format("2006-01-02 15:04:05"), "status": 1})
 		}
 		r.Close()
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 3)
 	}
 }
 func sendSmsMessage(mydb *Mydb) {
 	var cid string
 	for {
-		r := pool.Conn.Get()
-		_, err := r.Do("Select", 1)
-		if err != nil {
-			r.Close()
-			fmt.Println(err)
-			time.Sleep(time.Second * 2)
+		r, job, file, key := GetInfo("sms", "sms:message")
+		if r == nil {
+			time.Sleep(time.Second * 5)
 			continue
 		}
-		key, err := redis.String(r.Do("Rpop", "sms:message"))
-		if err != nil {
-			r.Close()
-			continue
-		}
-		content, err := redis.String(r.Do("Get", key))
-		if err != nil {
-			r.Close()
-			continue
-		}
-		var redisData map[string]string
-		err = json.Unmarshal([]byte(content), &redisData)
-		if err != nil {
-			r.Close()
-			continue
-		}
-		if _, ok := redisData["job"]; !ok {
-			r.Close()
-			continue
-		}
-		cid = redisData["cid"]
+		cid = strconv.Itoa(job.Cid)
 		cfg, err := redis.StringMap(r.Do("HgetAll", "wechat_config:"+cid))
 		if err != nil {
+			log.Info("[" + cid + "] 获取配置信息 失败. " + err.Error())
 			r.Close()
+			file.Close()
+			time.Sleep(time.Second * 3)
+			continue
+		}
+		_, err = r.Do("hIncrBy", "company:"+cid, "sms_lave", -1)
+		if err != nil {
+			r.Close()
+			log.Info("[" + cid + "] 获取商户信息 失败. " + err.Error())
+			file.Close()
+			time.Sleep(time.Second * 3)
 			continue
 		}
 		var sms *util.Mwsms
 		sms = util.NewMwsms("http://"+cfg["sms_ip"], cfg["sms_address"], cfg["sms_port"], cfg["sms_account"], cfg["sms_passcode"], "")
-		sendResult := sms.Send(redisData["phone"], redisData["job"], "*", time.Now().UnixNano(), 1)
+		sendResult := sms.Send(job.Phone, job.Job, "*", time.Now().UnixNano(), 1)
 		fmt.Println(sendResult)
 		if !sendResult {
-			r.Close()
+			r.Do("hIncrBy", "company:"+cid, "sms_lave")
+			log.Info("[" + cid + "] 发送短信 失败. " + err.Error())
+			time.Sleep(time.Second * 5)
 			mydb.db.Table("remind_job").Where("redis_key_index=?", key).Update(map[string]interface{}{"status": -1, "fail_content": err})
+			r.Close()
+			file.Close()
 			continue
+		} else {
+			log.Info("[" + cid + "] 发送短信 成功. ")
+			mydb.db.Table("remind_job").Where("redis_key_index=?", key).Update(map[string]interface{}{"action_at": time.Now().Format("2006-01-02 15:04:05"), "status": 1})
+			mydb.db.Table("r_smsrecord").Where("flag_content=?", key).Update(map[string]interface{}{"send_time": time.Now().Format("2006-01-02 15:04:05"), "status": 1})
+			// mydb.db.Table("company").Where("id=?", cid).Update(map[string]interface{}{"sms_slave": gorm.Expr("sms_lave-?", 1)})
 		}
-		mydb.db.Table("remind_job").Where("redis_key_index=?", key).Update(map[string]interface{}{"action_at": time.Now().Format("2006-01-02 15:04:05"), "status": 1})
+		r.Close()
+		file.Close()
+		time.Sleep(time.Second * 3)
 	}
 }
 
@@ -255,13 +281,13 @@ func init() {
 		panic(err)
 	}
 	pool = cache.NewRedis(&cache.RedisOpts{
-		Host:     cfg.Redis.Host,
-		Password: cfg.Redis.Auth,
-		Port:     cfg.Redis.Port,
-		Database: cfg.Redis.DB,
-		MaxActive:cfg.Redis.MaxActive,
-		MaxIdle:cfg.Redis.MaxIdle,
-		IdleTimeout:cfg.Redis.IdleTimeout,
+		Host:        cfg.Redis.Host,
+		Password:    cfg.Redis.Auth,
+		Port:        cfg.Redis.Port,
+		Database:    cfg.Redis.DB,
+		MaxActive:   cfg.Redis.MaxActive,
+		MaxIdle:     cfg.Redis.MaxIdle,
+		IdleTimeout: cfg.Redis.IdleTimeout,
 	})
 	mydb = NewMydb(cfg)
 }
